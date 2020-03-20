7e94c7906e6884da39f0452b8cf9e58d7a849989
hlz-xc-wy <1987800533@qq.com>
2020-03-20 22:06:25 +0800

    修改返回值

--- study_1/11/11-3.c
+++ study_1/11/11-3.c
@@ -5,14 +5,14 @@ void *
 thr_fn1(void *arg)
 {
 	printf("thread 1 returning\n");
-	return((void *)3);
+	return((void *)1);
 }
 
 void *
 thr_fn2(void *arg)
 {
 	printf("thread 2 exiting\n");
-	pthread_exit((void *)5);
+	pthread_exit((void *)2);
 }
 
 int
